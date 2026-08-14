2ad236dc6f2a7d7b8eb3499ff6dfbf5f3a6101bc
Dharmit <dharmitsathvara29@gmail.com>
2026-08-14 12:34:43 +0530

    Fix employee API route

--- models/Employee.js
+++ models/Employee.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const employeeSchema={
     name:{
         type:String,
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors=require('cors');
 const app=express();
 app.use(express.json());
 app.use(cors('*'));
-app.use("/api/employees",employeeRouter);
-app.listen(400,()=>{
+app.use("/employees",employeeRouter);
+app.listen(4000,()=>{
     console.log("Server is running on port 4000");
 });
