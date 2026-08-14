d8f1c68e4b53fb234c3447147b3d463426a39bba
Dharmit <dharmitsathvara29@gmail.com>
2026-08-14 10:41:27 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb+srv://dharmitsathvara29_db_user:AWgoML2qqWpI7wv4@cluster0.hdbvxbs.mongodb.net/?appName=Cluster0')
+
+const db=mongoose.connection;
+db.on("connected",()=>{
+    console.log("MongoDB connected successfully");
+});
+db.on("disconnected",()=>{
+    console.log("MongoDB disconnected ");
+});
+db.on("erroe",()=>{
+    console.log("MongoDB connection error:",error);
+});
+
+
+modeule.exports=db;
+
